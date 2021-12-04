567537b09d582e09717524173d7ed909b4c87cb7
KaloyanKoleve <kaloqnkolev555@gmail.com>
2021-12-04 17:47:05 +0200

    angular project upload

---
+++ angular2-skeleton/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular2-skeleton';
+  public list = [
+{
+  title: "Хидравлична преса",
+  url: "https://www.olx.bg/d/ad/hidravlichna-presa-za-izbivane-na-lageri-20-tona-s-butilkov-krik-300-95-CID1012-ID8HKxZ.html?reason=ip%7Ccf"
+},
+{
+  title: "Заваръчен апарат",
+  url: "https://etools.bg/product/21058/invertoren-kombiniran-elektrozhen-s-tig-hbm-4952-200-a.html"
+},
+{
+  title: "Динамометричен ключ 1",
+  url: "https://promachines.bg/product/10098/klyuch-dinamometrichen-1-2-70-350nm-force-6474661.html"
+},
+{
+  title: "Динамометричен ключ 2",
+  url: "https://www.onlinemashini.bg/avtomatichen-dinamometrichen-kliuch-mannesmann-18146/71295"
+},
+{
+  title: "Динамометричен ключ 3",
+  url: "https://www.onlinemashini.bg/kliu4-dinamometri4en-rodcraft-dms200--1-2-40200-nm-520-mm-/50493"
+},
+{
+  title: "Комбинирани звездогаечни ключове 1",
+  url: "https://www.olx.bg/d/ad/k-t-zvezdogaechni-treschotni-klyuchove-s-chupescho-ramo-ot-8-32mm-13-chasti-CID1012-ID8mkIO.html"
+},
+{
+  title: "Комбинирани звездогаечни ключове 2",
+  url: "https://toolsonline.bg/produkt/k-t-zvezdogaechni-chupeshti-treschotni-klyu/"
+},
+{
+  title: "Вакуум помпа",
+  url: "https://el-mashini.bg/product/5509/vakuum-pompa-kombinirana-za-vakuum-i-nalyagane-k-t.html"
+},
+  ]
+}
